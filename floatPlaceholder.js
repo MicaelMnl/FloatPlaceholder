7aa07a9b623fe19569dbb9acb0e3b87486beef55
micael <micael@liveencheres.com>
2017-07-20 17:05:24 +0200

    fix bugs

--- floatPlaceholder.js
+++ floatPlaceholder.js
@@ -1,24 +1,65 @@
-$(document).ready(function(){
+(function($){
+	$.fn.floatPlaceholder = function()
+    {
 
-    var $inputs = $('.float-placeholder-input'),
-        update = function(){
-            var $input = $(this),
-                $wrapper = $input.closest('.float-placeholder');
+        var methods =
+        {
+            init : function()
+            {
+                var  $inputs = $(this);
 
-            if( $input.val() !== '' || $input.is(':active') || $input.is(':focus') || $input.prop('tagName') === 'SELECT'){
-                $input.addClass('is-floating');
-            } else {
-                $input.removeClass('is-floating');
-            }
+               $inputs.each(function ()
+                {
+                    var $input = $(this);
 
-            if($input.is(':focus')){
-                $wrapper.addClass('is-focused');
-            } else {
-                $wrapper.removeClass('is-focused');
+                    if (['checkbox', 'radio'].indexOf($input.attr('type')) === -1)
+                    {
+                        var $parent = $input.parents('div').first().addClass('float-placeholder');
+
+                        var $label = $parent.find('label').first();
+                        $label.addClass('float-placeholder-label');
+
+                        $input.addClass('float-placeholder-input');
+
+                        $label.remove().clone().insertAfter($input);
+
+                        methods.update.apply($input);
+
+                        $input.on('click focus input blur', function(){
+                            methods.update.apply($(this))
+                        });
+                    }
+                });
+            },
+
+            update :  function()
+            {
+                var  $input = $(this);
+                var $wrapper = $input.closest('.float-placeholder');
+
+                if( $input.val() !== '' || $input.is(':active') || $input.is(':focus') || $input.prop('tagName') === 'SELECT'){
+                    $input.addClass('is-floating');
+                } else {
+                    $input.removeClass('is-floating');
+                }
+
+                if($input.is(':focus')){
+                    $wrapper.addClass('is-focused');
+                } else {
+                    $wrapper.removeClass('is-focused');
+                }
+            },
+
+            textarea : function()
+            {
+                var $textareas = $(this)
+                $textareas.addClass('float-placeholder-textarea')
             }
         };
 
-    $inputs.each( update );
-    $inputs.on('click focus input blur', update);
+        methods.init.apply($(this).find('input'));
+        methods.textarea.apply($(this).find('textarea'));
 
-});
+    };
+})(jQuery);
+
